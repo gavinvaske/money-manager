b829978ba8780a99091519fd64afb9d29b97e785
gavin vaske <Gavin.Vaske@gmail.com>
2020-10-17 15:41:13 -0500

    update report generation

--- moneymanager/__main__.py
+++ moneymanager/__main__.py
@@ -2,6 +2,10 @@ import pandas as pd
 import os
 from moneymanager.services.CsvManager import CsvManager
 
+IMPORT_TRANSACTIONS = True
+IMPORT_FROM_MULTIPLE_SOURCES = not IMPORT_TRANSACTIONS
+GENERATE_MONTHLY_REPORT = True
+
 if __name__ == '__main__':
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     input_directory = os.path.join(ROOT_DIR, 'data/input_files')
@@ -10,15 +14,18 @@ if __name__ == '__main__':
                 'DATE': 'object',
                 'DESCRIPTION': 'object',
                 'AMOUNT': 'float64',
-                'CATEGORY': 'object'
+                'CATEGORY': 'object',
+                'SOURCE': 'object'
     }
     CsvManager.preprocess_csv_files(input_directory)
     transactions = CsvManager.merge_csv_files(input_directory, dtypes)
     transactions['DATE'] = pd.to_datetime(transactions['DATE'])
     transactions = transactions.sort_values(by=['DATE'])
-
-    CsvManager.generate_monthly_data(transactions, output_directory)
     transactions.to_csv(os.path.join(output_directory, 'transactions.csv'), index=None)
+
+    if GENERATE_MONTHLY_REPORT:
+        CsvManager.generate_monthly_spending_reports(transactions, output_directory)
+
 
 
 
--- moneymanager/services/CsvManager.py
+++ moneymanager/services/CsvManager.py
@@ -15,6 +15,7 @@ class CsvManager:
             csv = pd.read_csv(file_path)
             csv = CsvManager.convert_column_names(csv)
             csv = CsvManager.add_source_column(csv, re.sub('\.csv$', '', file_name))
+            csv['CATEGORY'] = csv['CATEGORY'].fillna('UNCATEGORIZED').str.upper().str.strip()
             csv.to_csv(file_path, index=None)
 
     @staticmethod
@@ -27,7 +28,8 @@ class CsvManager:
 
     @staticmethod
     def add_source_column(csv, file_name, src_col_name='SOURCE'):
-        csv[src_col_name] = file_name
+        if src_col_name not in csv:
+            csv[src_col_name] = file_name
         return csv
 
 
@@ -39,19 +41,53 @@ class CsvManager:
         for file_name in file_names:
             csv = pd.read_csv(file_name, dtype=dtypes)
             dataframes.append(csv)
-
         return pd.concat(dataframes, axis=0, ignore_index=True)
 
     @staticmethod
-    def generate_monthly_data(csv, directory, date_col_name='DATE'):
-        col_month_year = 'month_year'
-        csv[col_month_year] = csv[date_col_name].dt.to_period('M')
-        csv_grouped = csv.groupby(col_month_year)
+    def generate_monthly_spending_reports(csv, directory, date_col_name='DATE', frequency='M'):
+        """
+        :param csv: pandas dataframe holding transactional data
+        :param directory: path to folder that will hold the generated files
+        :param frequency: a panda offset string (see https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#timeseries-offset-aliases)
+        :param date_col_name: name of the date column within the csv param
+        :return: None
+        """
+        period = 'period'
+        csv[period] = csv[date_col_name].dt.to_period(frequency)
+        csv_grouped = csv.groupby(period)
+        spending_by_category_list = []
+        unique_categories = csv['CATEGORY'].unique()
 
-        for group_name, df_group in csv_grouped:
-            df_group = df_group.drop(columns=[col_month_year])
+        for group_name, group in csv_grouped:
+            spending_dict = CsvManager.compute_spending_by_category(group, unique_categories)
+            spending_by_category_list.append(pd.Series(spending_dict, index=unique_categories, name=group_name))
+            group = group.drop(columns=[period])
             file_path = path.join(directory, str(group_name) + '.csv')
-            df_group.to_csv(file_path, index=None)
+            group.to_csv(file_path, index=None)
+
+        spending_by_category = pd.DataFrame(spending_by_category_list)
+        spending_by_category['NET CHANGE'] = round(spending_by_category.sum(axis=1), 2)
+        file_path = path.join(directory, 'spending_by_category' + '.csv')
+        spending_by_category.to_csv(file_path)
+
+    @staticmethod
+    def compute_spending_by_category(transactions, expense_categories):
+        spending_by_category = {}
+
+        for category in expense_categories:
+            spending_by_category[category] = 0
+
+        grouped_transactions = transactions.groupby('CATEGORY')
+
+        for group_name, group in grouped_transactions:
+            spending = round(group['AMOUNT'].sum(), 2)
+            spending_by_category[group_name] = spending
+        return spending_by_category
+
+
+
+
+
 
 
 
