5d548ac1636eadb3842e07238f7948b38880ddff
gavin vaske <Gavin.Vaske@gmail.com>
2020-10-17 10:57:41 -0500

    feat: combine data sources and output data grouped by month/year to csv files

--- moneymanager/__main__.py
+++ moneymanager/__main__.py
@@ -1,2 +1,24 @@
+import pandas as pd
+import os
+from moneymanager.services.CsvManager import CsvManager
+
 if __name__ == '__main__':
-    print('Hello world!!')
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    input_directory = os.path.join(ROOT_DIR, 'data/input_files')
+    output_directory = os.path.join(ROOT_DIR, 'data/output_files')
+    dtypes = {
+                'DATE': 'object',
+                'DESCRIPTION': 'object',
+                'AMOUNT': 'float64',
+                'CATEGORY': 'object'
+    }
+    CsvManager.preprocess_csv_files(input_directory)
+    transactions = CsvManager.merge_csv_files(input_directory, dtypes)
+    transactions['DATE'] = pd.to_datetime(transactions['DATE'])
+    transactions = transactions.sort_values(by=['DATE'])
+
+    CsvManager.generate_monthly_data(transactions, output_directory)
+    transactions.to_csv(os.path.join(output_directory, 'transactions.csv'), index=None)
+
+
+
---
+++ moneymanager/services/CsvManager.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import glob
+import os
+from os import path
+import re
+
+
+class CsvManager:
+    @staticmethod
+    def preprocess_csv_files(directory):
+        files_names = [f for f in os.listdir(directory) if path.isfile(path.join(directory, f))]
+
+        for file_name in files_names:
+            file_path = path.join(directory, file_name)
+            csv = pd.read_csv(file_path)
+            csv = CsvManager.convert_column_names(csv)
+            csv = CsvManager.add_source_column(csv, re.sub('\.csv$', '', file_name))
+            csv.to_csv(file_path, index=None)
+
+    @staticmethod
+    def convert_column_names(csv, to_upper=True):
+        if to_upper:
+            csv = csv.rename(str.upper, axis='columns')
+        else:
+            csv = csv.rename(str.lower, axis='columns')
+        return csv
+
+    @staticmethod
+    def add_source_column(csv, file_name, src_col_name='SOURCE'):
+        csv[src_col_name] = file_name
+        return csv
+
+
+    @staticmethod
+    def merge_csv_files(directory, dtypes):
+        file_names = glob.glob(directory + '/*.csv')
+        dataframes = []
+
+        for file_name in file_names:
+            csv = pd.read_csv(file_name, dtype=dtypes)
+            dataframes.append(csv)
+
+        return pd.concat(dataframes, axis=0, ignore_index=True)
+
+    @staticmethod
+    def generate_monthly_data(csv, directory, date_col_name='DATE'):
+        col_month_year = 'month_year'
+        csv[col_month_year] = csv[date_col_name].dt.to_period('M')
+        csv_grouped = csv.groupby(col_month_year)
+
+        for group_name, df_group in csv_grouped:
+            df_group = df_group.drop(columns=[col_month_year])
+            file_path = path.join(directory, str(group_name))
+            df_group.to_csv(file_path, index=None)
+
+
+
